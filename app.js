d1a247f8cbbefcd9151b954424405b6f9d6225ca
Chamon562 <Chamon562@gmail.com>
2021-01-30 22:02:01 -0800

    Add class Player and constructor with it since its going to be interacting with other elements in the canvas, and add arguments x, y, radius, color to the contructor because player is going to be a circle starting off.

--- app.js
+++ app.js
@@ -6,4 +6,23 @@ const canvas = document.querySelector("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 // grab the context so I can use its api to draw and do everything
-let ctx = canvas.getContext("2d");
+const ctx = canvas.getContext("2d");
+
+// to create a player consist of a circle in middle and once something hits it game over
+// payer needs to interact with the rest of elements on screen. 
+// look into creating a class for Player class Player capitol P to say its a class
+class Player{
+    // what properties does a player have? if its a circle Im gonna give it circle arguments. 
+    // a starting position for now, a size, and a color x, y, radius, color
+    // in order to create properties and specify to a specific class need to create a constrcutor
+    // a constructor is called each time I instantiate a new version of player class
+    // each time i create a new player will give player all these individual properties to differentiate it 
+    // from other players I might create
+    // whenever I create new players I add new proprerties on to that new instance of the player 
+    constructor(x, y, radius, color){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this. color = color;
+    }
+}
