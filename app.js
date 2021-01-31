e9dd2093c53c4a83d485056a8c0942882980cf8c
Chamon562 <Chamon562@gmail.com>
2021-01-30 21:49:05 -0800

    Add commit for creation of canvas, resizing canvas to fit to screen both width and height. Add variable for context set to ctx getContext to 2d.

---
+++ app.js
@@ -0,0 +1,9 @@
+// console.log("app.js conected") app is connected to the html
+// first grab the canvas and set it to a variable
+const canvas = document.querySelector("canvas");
+// console.log(canvas);
+// resizing canvas to make it fit to screen.
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+// grab the context so I can use its api to draw and do everything
+let ctx = canvas.getContext("2d");
